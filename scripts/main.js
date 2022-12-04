4ecdadc3e75d10d63cc5bce6f5b25c1b597bbed8
Dmitry Kontsevenko <dimanpm@yandex.ru>
2022-12-04 16:09:17 +0700

    Сверстать попап.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
-const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
 const popupForm = document.querySelector('.popup__form');
 const popupCover = document.querySelector('.popup__cover');
+const popupName = popupForm.querySelector('.popup__name');
+const popupDesc = popupForm.querySelector('.popup__description');
+const profile = document.querySelector('.profile');
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileDesc = document.querySelector('.profile__description');
+const elementsHeart = document.querySelectorAll('.element__heart');
 
 function openPopup(event) {
   event.preventDefault();
   popup.classList.add('popup_opened');
+  console.log({profile});
+  popupName.value = profileName.textContent;
+  popupDesc.value = profileDesc.textContent;
 }
 
 function closePopup() {
@@ -25,3 +34,24 @@ popupCover.addEventListener('click', function(event) {
 popupForm.addEventListener('click', function(event) {
     event.preventDefault();
 })
+
+elementsHeart.forEach(element => {
+  element.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log({element});
+  });
+})
+//addEventListener('click', changeHeart);
+
+function changeHeart(element) {
+  //event.preventDefault();
+  //console.log({element});
+  /*
+  elementHeart.classList.forEach(element => {
+    console.log(`classList.element = ${element}`);
+  });*/
+  /*
+  let arrElements = elementHeart.classList;
+  console.log(arrElements.contains('link'));*/
+  //console.log({elementHeart.classList});
+}

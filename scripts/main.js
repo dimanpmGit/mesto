2e50712195a8b257ce37e5d31c719b9dcc7a8fa6
Dmitry Kontsevenko <dimanpm@yandex.ru>
2022-12-21 12:53:07 +0700

    Создать 6 карточек js скриптом из массива инициализации.

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,65 @@ const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileDesc = document.querySelector('.profile__description');
 const saveButton = document.querySelector('.popup__save-button');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsContainer = document.querySelector('.elements__list');
+
+const createCard = (cardName, cardLink) => {
+  const li = document.createElement('li');
+  li.classList.add('element');
+  const img = document.createElement('img');
+  img.classList.add('element__image');
+  img.setAttribute('src', cardLink);
+  img.setAttribute('alt', cardName);
+  const div = document.createElement('div');
+  div.classList.add('element__info');
+  const h2 = document.createElement('h2');
+  h2.classList.add('element__name');
+  h2.textContent = cardName;
+  const button = document.createElement('button');
+  button.classList.add('element__heart-button');
+  button.setAttribute('type', 'button');
+  div.append(h2, button);
+  li.append(img, div);
+  return li;
+}
+
+const renderCard = (cardName, cardLink) => {
+  cardsContainer.append(createCard(cardName, cardLink));
+}
+
+cardsContainer.append(...initialCards.map((item) => {
+  return createCard(item.name, item.link);
+}))
+/*initialCards.forEach((item) => {
+  renderCard(item.name, item.link);
+})*/
 
 function openPopup(event) {
   event.preventDefault();

434ab5351dd14826fd60795d6811797236e16ce7
Dmitry Kontsevenko <dimanpm@yandex.ru>
2022-12-22 15:37:01 +0700

    5 спринт. 4.Лайк карточки реализован.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,12 @@
-/*
-const popup = document.querySelector('.popup');
-const popupContainer = document.querySelector('.popup__container');
-const popupName = popupContainer.querySelector('.popup__input_type_name');
-const popupDesc = popupContainer.querySelector('.popup__input_type_description');
-const profile = document.querySelector('.profile');
-*/
 const profileName = document.querySelector('.profile__name');
 const profileDesc = document.querySelector('.profile__description');
-const saveButton = document.querySelector('.popup__save-button');
-//const closePopupButton = document.querySelector('.popup__close-button');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 
 
+/*
+Инициализировать первые 6 карточек
+*/
 const initialCards = [
   {
     name: 'Архыз',
@@ -42,25 +36,11 @@ const initialCards = [
 
 const cardsContainer = document.querySelector('.elements__list');
 
+
+/*
+Создать одну карточку
+*/
 const createCard = (cardItem) => {
-  /*
-  const li = document.createElement('li');
-  li.classList.add('element');
-  const img = document.createElement('img');
-  img.classList.add('element__image');
-  img.setAttribute('src', cardLink);
-  img.setAttribute('alt', cardName);
-  const div = document.createElement('div');
-  div.classList.add('element__info');
-  const h2 = document.createElement('h2');
-  h2.classList.add('element__name');
-  h2.textContent = cardName;
-  const button = document.createElement('button');
-  button.classList.add('element__heart-button');
-  button.setAttribute('type', 'button');
-  div.append(h2, button);
-  li.append(img, div);
-  return li;*/
   const string = `<li class="element">
       <img class="element__image">
       <div class="element__info">
@@ -79,6 +59,15 @@ const createCard = (cardItem) => {
   img.setAttribute('alt', cardItem.name);
   heartButton.setAttribute('type', 'button');
 
+  //Обработка событий
+  /*
+    Лайк карточки
+  */
+  heartButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    heartButton.classList.toggle('element__heart-button_active');
+  })
+
   return container.firstElementChild;
 }
 
@@ -90,20 +79,25 @@ cardsContainer.append(...initialCards.map((item) => {
   return createCard(item);
 }))
 
+let elementsHeart = cardsContainer.querySelectorAll('.element__heart-button');
+
+/*
+Инициализировать попапы начальными значениями
+*/
 const initialPopups = [
   {
     type: 'popup__container_edit-profile',
+    formName: 'popup__form_edit-profile',
     title: 'Редактировать профиль',
     buttonName: 'Сохранить',
-    //initialiseInput: 'true',
     placeholderTop: 'Имя',
     placeholderBottom: 'Описание'
   },
   {
     type: 'popup__container_add-card',
+    formName: 'popup__form_add-card',
     title: 'Новое место',
     buttonName: 'Сохранить',
-    //initialiseInput: null,
     placeholderTop: 'Название',
     placeholderBottom: 'Ссылка на страницу'
   }
@@ -111,11 +105,14 @@ const initialPopups = [
 
 const popup = document.querySelector('.popup');
 
+/*
+Создать все попапы
+*/
 const createPopup = (popupItem) => {
   const string = `<div class="popup__container popup__container_hidden">
       <button class="popup__close-button link" type="button"></button>
       <h2 class="popup__title"></h2>
-      <form class="popup__form" name="popup-form">
+      <form class="popup__form">
         <input class="popup__input popup__input_type_name" name="popup-name" type="text" placeholder="Имя">
         <input class="popup__input popup__input_type_description" name="popup-description" type="text"
           placeholder="Описание">
@@ -125,10 +122,13 @@ const createPopup = (popupItem) => {
   const container = document.createElement('div');
   container.innerHTML = string;
   const popupContainer = container.querySelector('.popup__container');
+  const formName = container.querySelector('.popup__form');
   const title = container.querySelector('.popup__title');
   const inputTop = container.querySelector('.popup__input_type_name');
   const inputBottom = container.querySelector('.popup__input_type_description');
+  
   popupContainer.setAttribute('id', popupItem.type);
+  formName.setAttribute('name', popupItem.formName);
   title.textContent = popupItem.title;
   inputTop.setAttribute('placeholder', popupItem.placeholderTop);
   inputBottom.setAttribute('placeholder', popupItem.placeholderBottom);
@@ -145,19 +145,15 @@ popup.append(...initialPopups.map((item) => {
 }))
 
 const closePopupButtons = popup.querySelectorAll('.popup__close-button');
-/*initialCards.forEach((item) => {
-  renderCard(item.name, item.link);
-})*/
 
-
-
+/*
+Открыть попап
+*/
 function openPopup(event) {
   event.preventDefault();
   popup.classList.toggle('popup_opened');
   if (event.target.className.includes('profile__edit-button')) {
     popup.querySelector('#popup__container_edit-profile').classList.remove('popup__container_hidden');
-    //const inputTop = event.target.querySelector('.popup__input_type_name');
-    //const inputBottom = event.target.querySelector('.popup__input_type_description');
     popup.querySelector('.popup__input_type_name').value = profileName.textContent;
     popup.querySelector('.popup__input_type_description').value = profileDesc.textContent;
     
@@ -165,12 +161,11 @@ function openPopup(event) {
   else if (event.target.className.includes('profile__add-button')) {
     popup.querySelector('#popup__container_add-card').classList.remove('popup__container_hidden');
   }
-  //const popupContainer = popup.querySelector('.popup__container');
-  //popupContainer.classList.toggle('popup_hidden');
-  //popupName.value = profileName.textContent;
-  //popupDesc.value = profileDesc.textContent;
 }
 
+/*
+Закрыть попап
+*/
 function closePopup() {
   popup.classList.toggle('popup_opened');
   popup.querySelectorAll('.popup__container').forEach(element => {
@@ -180,13 +175,10 @@ function closePopup() {
 
 // Находим форму в DOM
 const formElements = document.querySelectorAll('.popup__form');// Воспользуйтесь методом querySelector()
+
 /*
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('.popup__input_type_description');// Воспользуйтесь инструментом .querySelector()
+ Обработчик «отправки» формы
 */
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (event) {
   event.preventDefault();
   const inputTop = event.target.querySelector('.popup__input_type_name');
@@ -201,7 +193,9 @@ function handleFormSubmit (event) {
       link: inputBottom.value
     }
     cardsContainer.prepend(createCard(cardItem));
+    elementsHeart = cardsContainer.querySelectorAll('.element__heart-button');
   }
+  //this.submit();
   inputTop.value = '';
   inputBottom.value = '';
   closePopup();
@@ -227,3 +221,5 @@ addButton.addEventListener('click', openPopup);
 closePopupButtons.forEach(element => {
   element.addEventListener('click', closePopup);
 });
+
+
